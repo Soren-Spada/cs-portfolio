e9313f0502de7cefec97a18f11f8b30167e7620c
soren_spada <sspada.sh@mysummitps.org>
2016-01-14 23:26:23 +0000

    committed

---
+++ infosite.js
@@ -0,0 +1,74 @@
+// JavaScript File
+var item1 = document.getElementById("item1");
+var item2 = document.getElementById("item2");
+var item3 = document.getElementById("item3");
+var item4 = document.getElementById("item4");
+var item5 = document.getElementById("item5");
+var item6 = document.getElementById("item6");
+var item7 = document.getElementById("item7");
+var item8 = document.getElementById("item8");
+var item9 = document.getElementById("item9");
+var item10 = document.getElementById("item10");
+
+
+var info1 = document.getElementById("info1");
+var info2 = document.getElementById("info2");
+var info3 = document.getElementById("info3");
+var info4 = document.getElementById("info4");
+var info5 = document.getElementById("info5");
+var info6 = document.getElementById("info6");
+var info7 = document.getElementById("info7");
+var info8 = document.getElementById("info8");
+var info9 = document.getElementById("info9");
+var info10 = document.getElementById("info10");
+
+
+
+item1.addEventListener("click", function(){
+    info1.innerHTML = "Most of the csgo skins with reptiles and monsters look super cool. The ak is probably the most useful firarms.";
+});
+
+
+item2.addEventListener("click", function(){
+    info2.innerHTML = "One, its a knife. Two its marable fade. Three it is the swaggiest thing to kill your enemies with";
+});
+
+
+item3.addEventListener("click", function(){
+    info3.innerHTML = "My favorite weapon from the Gods and Monsters Collection. Kind of reminds me of the blue eyes white dragon from Yu-Gi-Oh";
+});
+
+
+item4.addEventListener("click", function(){
+    info4.innerHTML = "There are like 2 guns in the entire game with and entire white skin. It stands out so much and inspecting this weapon with the contrast of the environment color looks super cool.";
+});
+
+
+item5.addEventListener("click", function(){
+    info5.innerHTML = "I like anime, and this gun shows just that. Although this gun already gets hate with the nickname 'COD gun', this one gets even more hate.";
+});
+
+
+item6.addEventListener("click", function(){
+    info6.innerHTML = "The awp graphite is the most plain looking skins, but when in game, it looks beautiful. Any skins added on to it looks really good.";
+});
+
+
+item7.addEventListener("click", function(){
+    info7.innerHTML = "Titan holo stickers on the p2k ocean foam is the greatest looking skin. It looks like you brought a beach to a gun.";
+});
+
+
+item8.addEventListener("click", function(){
+    info8.innerHTML = "Its the pepe bizon with its pepe green color. This wilol truly make your weapons 'feel bad man'.";
+});
+
+
+item9.addEventListener("click", function(){
+    info9.innerHTML = "The orion looks super basic, but is actually really unique. Something about the dots along with the orange and black contrast.";
+});
+
+
+item10.addEventListener("click", function(){
+    info10.innerHTML = "If you have the urge to mawl someone in counter-strike, the only way to do it is through the M4 howl. The only 'contraband skin in the game, its red shade design makes it the coolest looking skin in the game.";
+});
