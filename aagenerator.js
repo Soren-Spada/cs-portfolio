08ee6e0e721054b0aee369b71cd7a1178d73ca58
soren_spada <sspada.sh@mysummitps.org>
2016-06-03 21:56:01 +0000

    updaito

---
+++ aagenerator.js
@@ -0,0 +1,30 @@
+// JavaScript File
+    
+
+
+var vid1 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/cIYRlqPddnk" frameborder="0" allowfullscreen></iframe>';
+var vid2 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/RBc0A7ZPv00" frameborder="0" allowfullscreen></iframe>';
+var vid3 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/bv-vGXaNCHY" frameborder="0" allowfullscreen></iframe>';
+var vid4 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/izEDbzTBp0k" frameborder="0" allowfullscreen></iframe>';
+var vid5 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/6rpMhcGlEeo" frameborder="0" allowfullscreen></iframe>';
+var vid6 = '<iframe width="560" height="315" src="https://www.youtube.com/embed/ic1BmH-VQBA" frameborder="0" allowfullscreen></iframe>';
+var vid7 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/74V_KBNZL5w" frameborder="0" allowfullscreen></iframe>';
+var vid8 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/wgYXPHCOopc" frameborder="0" allowfullscreen></iframe>';
+var vid9 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/ckzk2jZ65wk" frameborder="0" allowfullscreen></iframe>';
+var vid10 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/4B86YnEKbM0" frameborder="0" allowfullscreen></iframe>';
+var vid11 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/Mb3YYd-_IVc" frameborder="0" allowfullscreen></iframe>';
+var vid12 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/A-11TNPSp1U" frameborder="0" allowfullscreen></iframe>';
+var vid13 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/jTpCgJ-FNbU" frameborder="0" allowfullscreen></iframe>';
+var vid14 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/IN8nNdPqlN0" frameborder="0" allowfullscreen></iframe>';
+var vid15 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/zIleQ8S6Zko" frameborder="0" allowfullscreen></iframe>';
+var vid16 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/1wEsgjBiME0" frameborder="0" allowfullscreen></iframe>';
+var vid17 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/LvJhacYIJK4" frameborder="0" allowfullscreen></iframe>';
+var vid18 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/FAagWWQwB5M" frameborder="0" allowfullscreen></iframe>';
+var vid19 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/cnVSd640I90" frameborder="0" allowfullscreen></iframe>';
+var vid20 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/M00RDg1UbxM" frameborder="0" allowfullscreen></iframe>';
+var vid21 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/KdKZAGMo9J4" frameborder="0" allowfullscreen></iframe>';
+var vid22 = '<iframe width="420" height="315" src="https://www.youtube.com/embed/ojr7MWuJ90E" frameborder="0" allowfullscreen></iframe>';
+var myVideo = [ vid1, vid2, vid3, vid4, vid5, vid6, vid7, vid8, vid9, vid10, vid11, vid12, vid13, vid14, vid15, vid16, vid17, vid18, vid19, vid20, vid21, vid22]
+
+
+var randVideo = myVideo[Math.floor(Math.random() * myVideo.length)];
--- modeling.js
+++ modeling.js
@@ -1,1 +1,81 @@
 // JavaScript File
+var riceBag = document.getElementById("riceBag");
+var riceMeasure = document.getElementById("riceMeasure");
+var riceWash = document.getElementById("riceWash");
+var riceStrain = document.getElementById("riceStrain");
+var ricePot = document.getElementById("ricePot");
+var waterMeasure = document.getElementById("waterMeasure");
+var waterPour = document.getElementById("waterPour");
+var riceStove1 = document.getElementById("riceStove1");
+var riceStove2 = document.getElementById("riceStove2");
+var riceTimer = document.getElementById("riceTimer");
+var riceBowl = document.getElementById("riceBowl");
+
+// Using this click function, this picture along with the text will disappear, and the new div filled with text and pictures will pop up.
+riceBag.addEventListener("click", function(){
+    riceMeasure.className = "";
+    riceBag.className = "hideMe";
+});
+
+riceMeasure.addEventListener("drop", function(){
+    riceWash.className = "";
+    riceMeasure.className = "hideMe";
+    
+});
+
+riceWash.addEventListener("drop", function(){
+    riceStrain.className = "";
+    riceWash.className = "hideMe";
+});
+
+riceStrain.addEventListener("drop", function(){
+    ricePot.className = "";
+    riceStrain.className = "hideMe";
+});
+
+ricePot.addEventListener("drop", function(){
+    waterMeasure.className = "";
+    ricePot.className = "hideMe";
+});
+
+waterMeasure.addEventListener("drop", function(){
+    waterPour.className = "";
+    waterMeasure.className = "hideMe";
+});
+
+waterPour.addEventListener("drop", function(){
+    riceStove1.className = "";
+    waterPour.className = "hideMe";
+});
+
+riceStove1.addEventListener("click", function(){
+    riceStove2.className = "";
+    riceStove1.className = "hideMe";
+});
+
+riceStove2.addEventListener("click", function(){
+    riceTimer.className = "";
+    riceStove2.className = "hideMe";
+});
+
+riceTimer.addEventListener("click", function(){
+    riceBowl.className = "";
+    riceTimer.className = "hideMe";
+});
+
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    // ev.target.src = ""
+}
